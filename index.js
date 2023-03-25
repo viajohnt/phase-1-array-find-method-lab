de073bf3eb1b0b062849b8cda04ebb243de4e9ca
John Truong <viajohnt@gmail.com>
2023-03-25 16:23:04 -0600

    Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
+
+superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W");
+    return !!result? result.year : undefined;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
